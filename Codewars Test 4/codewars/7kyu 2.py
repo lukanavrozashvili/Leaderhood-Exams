2ddf0322913c0b4c261f5de5e7b0794547f62574
Luka Navrozashvili <143267861+lukanavrozashvili@users.noreply.github.com>
2024-08-18 16:26:59 +0400

    commit

---
+++ Codewars Test 4/codewars/7kyu 2.py
@@ -0,0 +1,9 @@
+# def is_divisible(n, *args):
+#     if len(args) == 0:
+#         return True
+    
+#     for num in args:
+#         if n % num != 0:
+#             return False
+        
+#     return True
